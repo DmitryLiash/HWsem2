d5d8dbe8058f8dde4c84cc56fd622e20940b7a25
DmitryL <dimak.pax@gmail.com>
2022-12-25 12:35:28 +0600

    002 v01

---
+++ 002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string strNumber = Convert.ToString(Number);
+if (strNumber.Length > 2)
+{
+  Console.WriteLine(strNumber[2] + " третья цифра");
+}
+else 
+{
+  Console.WriteLine("третьей цифры нет");
+}
