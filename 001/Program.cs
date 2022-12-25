ec72bafe79aeb417eefef3ce246fc76278923e07
DmitryL <dimak.pax@gmail.com>
2022-12-25 12:22:04 +0600

    001 ok

---
+++ 001/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine(stringNumber[1] + " Вторая цифра введеного трехзначного числа");
